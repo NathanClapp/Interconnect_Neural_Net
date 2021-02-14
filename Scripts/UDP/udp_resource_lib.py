da0a2fd04b8ba4547f175d3f039893cdf9ed87ec
NathanClapp <gnathanq20@gmail.com>
2021-02-14 09:16:57 -0700

    added import mechanism for custom py modules

--- Scripts/UDP/udp_resource_lib.py
+++ Scripts/UDP/udp_resource_lib.py
@@ -2,11 +2,16 @@
 
 #normal python modules
 import subprocess, multiprocessing, sys, os, json, types
-from inspect import signature
+from inspect import signature, getmembers
 
 #get this path automatically later
 operator_root_dir = '/home/nathan/Documents/Code/Interconnect_Neural_Net/Scripts/Layer_Programs/Operators'
 #json_module_dir = 
+
+#
+def py_modules(module_name_string):
+    modules_list = []
+    modules_list.append(sys.modules[module_name])
 
 #import files in the given directory as modules
 #return list of modules
@@ -22,8 +27,6 @@ def py_modules_from_directory(operator_dir):
             module_name = file.replace(file_suffix, '')
             exec(exec_path_append + 'import ' + module_name)
             modules_list.append(sys.modules[module_name])
-        else:
-            continue
     return modules_list
 
 operator_modules = py_modules_from_directory(operator_dir=operator_root_dir)
@@ -32,25 +35,33 @@ print(operator_modules)
 #workspot
 #def module_object_to_string(module_name):
 
-def nested_dict_template(module_name_string, function, function_argc):
-    return {module_name_string : {function : function_argc} }
+def nested_data_template(module_name_string, function, function_argc):
+    return [ module_name_string, [function,  function_argc] ]
 
-print(nested_dict_template(operator_modules[0], 'testfuncstr', 5))
+print(nested_data_template(operator_modules[0], 'testfuncstr', 5))
 print(dir(operator_modules[0]))
 
-#get every function and the number of inputs for each
+#get every function and the number of inputs for each, for all modules in modules list
 #return a structured json array of module_name(function_name(input_count))
 #ignore builtin methods by default
-def py_functions_from_modules(modules_list, json_dir, ignore_substring='__'):
+def py_functions_from_modules(modules_list, ignore_substring='__'):
+    function_cumulative_argc = 0
+    function_argc_dict = {}
     #open json and prepare for writing
-
     for module_name in modules_list:
         for function in dir(module_name):
             #ignore functions beginning or ending in '__', such as __init__
             if not (function.startswith(ignore_substring) or function.endswith(ignore_substring)):
-                function_argc = len(signature(function).parameters)
-    #workspot
-    return 0;
+                #find number of parameters each function takes
+                function_argc = len(signature(getattr(module_name, function)).parameters)
+                #total parameter count
+                function_cumulative_argc += function_argc
+                function_argc_dict.update({function : function_argc})
+    return [function_argc_dict, function_cumulative_argc]
+
+print(py_functions_from_modules(operator_modules))
+
+
 
 #returns python list of ints
 def parse_tensor(decoded_tensor):
